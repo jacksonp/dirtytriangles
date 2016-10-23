c040cd8c75af9f11fac55b725f807f27dd2590bb
Jackson Pauls <jacksonpauls@gmail.com>
2016-10-23 09:37:47 +0100

    Use react-icons for links in footer.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,18 @@
 require('./index.css');
 
 import React from 'react'
+import FaTwitter from 'react-icons/lib/fa/twitter'
+import FaEnvelopeO from 'react-icons/lib/fa/envelope-o'
+import FaGithubAlt from 'react-icons/lib/fa/github-alt'
 
 const Footer = () => (
     <footer id="dt-footer">
-        <a href="https://github.com/jacksonp/dirtytriangles">GitHub</a>
-        <a href="https://twitter.com/DirtyTriangles">Twitter</a>
-        <a href="mailto:jacksonpauls@gmail.com">Email</a>
-        v1.0
+        <div className="links">
+            <a href="https://github.com/jacksonp/dirtytriangles"><FaGithubAlt width="30" height="30"/></a>
+            <a href="https://twitter.com/DirtyTriangles"><FaTwitter width="30" height="30"/></a>
+            <a href="mailto:jacksonpauls@gmail.com"><FaEnvelopeO width="30" height="30"/></a>
+        </div>
+        <span className="version">v1.0</span>
     </footer>
 );
 
