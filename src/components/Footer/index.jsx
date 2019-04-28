d38276afc032d5fcb5d6d2d6646d42dd0a52ea1a
Jackson Pauls <jacksonpauls@gmail.com>
2019-04-28 12:54:39 +0100

    Remove twitter link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,12 @@
 import './index.css';
 
 import React from 'react'
-import {FaTwitter, FaEnvelopeOpen, FaGithubAlt }from 'react-icons/fa'
+import { FaEnvelopeOpen, FaGithubAlt } from 'react-icons/fa'
 
 const Footer = () => (
     <footer id="dt-footer">
         <div className="links">
             <a href="https://github.com/jacksonp/dirtytriangles" title="GitHub"><FaGithubAlt width="30" height="30"/></a>
-            <a href="https://twitter.com/DirtyTriangles" title="Twitter"><FaTwitter width="30" height="30"/></a>
             <a href="mailto:jacksonpauls@gmail.com" title="Email"><FaEnvelopeOpen width="30" height="30"/></a>
         </div>
         <span className="version">v1.0</span>
